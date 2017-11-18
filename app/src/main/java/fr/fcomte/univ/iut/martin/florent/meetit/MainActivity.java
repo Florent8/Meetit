0d2dcc826c216dc668c18290374d83d5ba3bce0c
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-11-18 18:27:44 +0100

    Chapitre 1

--- app/src/main/java/fr/fcomte/univ/iut/martin/florent/meetit/MainActivity.java
+++ app/src/main/java/fr/fcomte/univ/iut/martin/florent/meetit/MainActivity.java
@@ -1,10 +1,9 @@
 package fr.fcomte.univ.iut.martin.florent.meetit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,89 +12,98 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.action_settings;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.drawer_layout;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.fab;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_camera;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_gallery;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_manage;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_send;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_share;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_slideshow;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.nav_view;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.id.toolbar;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.layout.activity_main;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.menu.main;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.string.navigation_drawer_close;
+import static fr.fcomte.univ.iut.martin.florent.meetit.R.string.navigation_drawer_open;
+
+public final class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(activity_main);
+        final Toolbar bar = findViewById(toolbar);
+        setSupportActionBar(bar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        findViewById(fab).setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        final DrawerLayout drawer = findViewById(drawer_layout);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, bar, navigation_drawer_open, navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        ((NavigationView) findViewById(nav_view)).setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        final DrawerLayout drawer = findViewById(drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        else
             super.onBackPressed();
-        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == action_settings)
             return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull final MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case nav_camera:
+                // Handle the camera action
+                break;
+            case nav_gallery:
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+                break;
+            case nav_slideshow:
 
-        } else if (id == R.id.nav_slideshow) {
+                break;
+            case nav_manage:
 
-        } else if (id == R.id.nav_manage) {
+                break;
+            case nav_share:
 
-        } else if (id == R.id.nav_share) {
+                break;
+            case nav_send:
 
-        } else if (id == R.id.nav_send) {
-
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        ((DrawerLayout) findViewById(drawer_layout)).closeDrawer(GravityCompat.START);
         return true;
     }
 }
